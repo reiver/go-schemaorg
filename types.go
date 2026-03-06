b91d7f7479f1f1a777d7cd9d17007886492e3b21
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-06 13:46:31 -0800

    types

--- types.go
+++ types.go
@@ -1,8 +1,22 @@
 package schemaorg
 
 const (
+	TypeAboutPage         = "AboutPage"
+	TypeAction            = "Action"
 	TypeAnswer            = "Answer"
+	TypeCheckoutPage      = "CheckoutPage"
+	TypeCollectionPage    = "CollectionPage"
 	TypeComment           = "Comment"
+	TypeContactPage       = "ContactPage"
 	TypeCorrectionComment = "CorrectionComment"
+	TypeComputerLanguage  = "ComputerLanguage"
+	TypeFAQPage           = "FAQPage"
+	TypeItemPage          = "ItemPage"
+	TypeMedicalWebPage    = "MedicalWebPage"
+	TypeProfilePage       = "ProfilePage"
+	TypeQAPage            = "QAPage"
 	TypeQuestion          = "Question"
+	TypeRealEstateListing = "RealEstateListing"
+	TypeSearchResultsPage = "SearchResultsPage"
+	TypeWebPage           = "WebPage"
 )
