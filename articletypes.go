7eff31c5c517f3e5452b0fcc9f4fb39067849b25
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-06 14:56:59 -0800

    types

---
+++ articletypes.go
@@ -0,0 +1,21 @@
+package schemaorg
+
+const (
+	TypeAnalysisNewsArticle     = "AnalysisNewsArticle"
+	TypeAPIReference            = "APIReference"
+	TypeArticle                 = "Article"
+	TypeAskPublicNewsArticle    = "AskPublicNewsArticle"
+	TypeBackgroundNewsArticle   = "BackgroundNewsArticle"
+	TypeBlogPosting             = "BlogPosting"
+	TypeDiscussionForumPosting  = "DiscussionForumPosting"
+	TypeMedicalScholarlyArticle = "MedicalScholarlyArticle"
+	TypeNewsArticle             = "NewsArticle"
+	TypeOpinionNewsArticle      = "OpinionNewsArticle"
+	TypeReport                  = "Report"
+	TypeReportageNewsArticle    = "ReportageNewsArticle"
+	TypeReviewNewsArticle       = "ReviewNewsArticle"
+	TypeSatiricalArticle        = "SatiricalArticle"
+	TypeScholarlyArticle        = "ScholarlyArticle"
+	TypeSocialMediaPosting      = "SocialMediaPosting"
+	TypeTechArticle             = "TechArticle"
+)
