e22dbb0f0d6a6064e5351b4b56e44ed38fcaeb09
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-06 14:05:03 -0800

    types

---
+++ commenttypes.go
@@ -0,0 +1,8 @@
+package schemaorg
+
+const (
+	TypeAnswer            = "Answer"
+	TypeComment           = "Comment"
+	TypeCorrectionComment = "CorrectionComment"
+	TypeQuestion          = "Question"
+)
