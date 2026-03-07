da898d23c8712315731d6319836c4466d5fe8ba7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-06 18:09:08 -0800

    news article

---
+++ newsarticle_marshal_test.go
@@ -0,0 +1,110 @@
+package schemaorg_test
+
+import (
+	"testing"
+
+	"bytes"
+
+	"github.com/reiver/go-jsonld"
+	"github.com/reiver/go-opt"
+
+	"github.com/reiver/go-schemaorg"
+)
+
+func TestNewsArticle_marshal(t *testing.T) {
+
+	const context string = `"@context":{"schema":"https://schema.org/","@id":"schema:@id","@type":"schema:@type","about":"schema:about","abstract":"schema:abstract","accessMode":"schema:accessMode","accessModeSufficient":"schema:accessModeSufficient","accessibilityAPI":"schema:accessibilityAPI","accessibilityControl":"schema:accessibilityControl","accessibilityFeature":"schema:accessibilityFeature","accessibilityHazard":"schema:accessibilityHazard","accessibilitySummary":"schema:accessibilitySummary","accountablePerson":"schema:accountablePerson","acquireLicensePage":"schema:acquireLicensePage","additionalType":"schema:additionalType","aggregateRating":"schema:aggregateRating","alternateName":"schema:alternateName","alternativeHeadline":"schema:alternativeHeadline","archivedAt":"schema:archivedAt","articleBody":"schema:articleBody","articleSection":"schema:articleSection","assesses":"schema:assesses","associatedMedia":"schema:associatedMedia","audience":"schema:audience","audio":"schema:audio","author":"schema:author","award":"schema:award","backstory":"schema:backstory","character":"schema:character","citation":"schema:citation","comment":"schema:comment","commentCount":"schema:commentCount","conditionsOfAccess":"schema:conditionsOfAccess","contentLocation":"schema:contentLocation","contentRating":"schema:contentRating","contentReferenceTime":"schema:contentReferenceTime","contributor":"schema:contributor","copyrightHolder":"schema:copyrightHolder","copyrightNotice":"schema:copyrightNotice","copyrightYear":"schema:copyrightYear","correction":"schema:correction","countryOfOrigin":"schema:countryOfOrigin","creativeWorkStatus":"schema:creativeWorkStatus","creator":"schema:creator","creditText":"schema:creditText","dateCreated":"schema:dateCreated","dateModified":"schema:dateModified","datePublished":"schema:datePublished","dateline":"schema:dateline","description":"schema:description","digitalSourceType":"schema:digitalSourceType","disambiguatingDescription":"schema:disambiguatingDescription","discussionUrl":"schema:discussionUrl","displayLocation":"schema:displayLocation","editEIDR":"schema:editEIDR","editor":"schema:editor","educationalAlignment":"schema:educationalAlignment","educationalLevel":"schema:educationalLevel","educationalUse":"schema:educationalUse","encoding":"schema:encoding","encodingFormat":"schema:encodingFormat","exampleOfWork":"schema:exampleOfWork","expires":"schema:expires","funder":"schema:funder","funding":"schema:funding","genre":"schema:genre","hasPart":"schema:hasPart","headline":"schema:headline","identifier":"schema:identifier","image":"schema:image","inLanguage":"schema:inLanguage","interactionStatistic":"schema:interactionStatistic","interactivityType":"schema:interactivityType","interpretedAsClaim":"schema:interpretedAsClaim","isAccessibleForFree":"schema:isAccessibleForFree","isBasedOn":"schema:isBasedOn","isFamilyFriendly":"schema:isFamilyFriendly","isPartOf":"schema:isPartOf","keywords":"schema:keywords","learningResourceType":"schema:learningResourceType","license":"schema:license","locationCreated":"schema:locationCreated","mainEntity":"schema:mainEntity","mainEntityOfPage":"schema:mainEntityOfPage","maintainer":"schema:maintainer","material":"schema:material","materialExtent":"schema:materialExtent","mentions":"schema:mentions","name":"schema:name","offers":"schema:offers","owner":"schema:owner","pageEnd":"schema:pageEnd","pageStart":"schema:pageStart","pagination":"schema:pagination","pattern":"schema:pattern","position":"schema:position","potentialAction":"schema:potentialAction","printColumn":"schema:printColumn","printEdition":"schema:printEdition","printSection":"schema:printSection","producer":"schema:producer","provider":"schema:provider","publication":"schema:publication","publisher":"schema:publisher","publisherImprint":"schema:publisherImprint","publishingPrinciples":"schema:publishingPrinciples","recordedAt":"schema:recordedAt","releasedEvent":"schema:releasedEvent","review":"schema:review","sameAs":"schema:sameAs","schemaVersion":"schema:schemaVersion","sdDatePublished":"schema:sdDatePublished","sdLicense":"schema:sdLicense","sdPublisher":"schema:sdPublisher","size":"schema:size","sourceOrganization":"schema:sourceOrganization","spatial":"schema:spatial","spatialCoverage":"schema:spatialCoverage","speakable":"schema:speakable","sponsor":"schema:sponsor","subjectOf":"schema:subjectOf","teaches":"schema:teaches","temporal":"schema:temporal","temporalCoverage":"schema:temporalCoverage","text":"schema:text","thumbnail":"schema:thumbnail","thumbnailUrl":"schema:thumbnailUrl","timeRequired":"schema:timeRequired","translationOfWork":"schema:translationOfWork","translator":"schema:translator","typicalAgeRange":"schema:typicalAgeRange","url":"schema:url","usageInfo":"schema:usageInfo","version":"schema:version","video":"schema:video","wordCount":"schema:wordCount","workExample":"schema:workExample","workTranslation":"schema:workTranslation"}`
+
+	type EmbeddedNewsArticle struct {
+		NewsArticle schemaorg.NewsArticle `json:"newsArticle,omitempty"`
+	}
+
+	tests := []struct{
+		Value any
+		Expected []byte
+	}{
+		{
+			Value: schemaorg.NewsArticle{},
+			Expected: []byte(`{`+context+`,"@type":"NewsArticle"}`),
+		},
+		{
+			Value: EmbeddedNewsArticle{},
+			Expected: []byte(`{`+context+`}`),
+		},
+
+
+
+		{
+			Value: EmbeddedNewsArticle{
+				NewsArticle: schemaorg.NewsArticle{
+
+					Name: opt.Something("City Council Approves New Park"),
+					URL:  opt.Something("https://example.com/news/city-council-park"),
+
+					Author:        opt.Something("John Reporter"),
+					DatePublished: opt.Something("2024-08-20"),
+					Headline:      opt.Something("City Council Approves New Park Development"),
+
+					ArticleBody:    opt.Something("The city council voted unanimously to approve..."),
+					ArticleSection: opt.Something("Local News"),
+
+					Dateline:     opt.Something("NEW YORK, Aug. 20"),
+					PrintColumn:  opt.Something("3"),
+					PrintEdition: opt.Something("Morning Edition"),
+					PrintSection: opt.Something("A"),
+				},
+			},
+			Expected: []byte(
+				`{`+
+					context+
+					`,`+
+					`"newsArticle":{`+
+						`"@type":"NewsArticle"`+
+						`,`+
+						`"name":"City Council Approves New Park"`+
+						`,`+
+						`"url":"https://example.com/news/city-council-park"`+
+						`,`+
+						`"author":"John Reporter"`+
+						`,`+
+						`"datePublished":"2024-08-20"`+
+						`,`+
+						`"headline":"City Council Approves New Park Development"`+
+						`,`+
+						`"articleBody":"The city council voted unanimously to approve..."`+
+						`,`+
+						`"articleSection":"Local News"`+
+						`,`+
+						`"dateline":"NEW YORK, Aug. 20"`+
+						`,`+
+						`"printColumn":"3"`+
+						`,`+
+						`"printEdition":"Morning Edition"`+
+						`,`+
+						`"printSection":"A"`+
+					`}`+
+				`}`),
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		actual, err := jsonld.Marshal(test.Value)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one.", testNumber)
+			t.Logf("ERROR: (%T) %s", err, err)
+			t.Logf("VALUE: (%T) %#v", test.Value, test.Value)
+			continue
+		}
+
+		expected := test.Expected
+
+		if !bytes.Equal(expected, actual) {
+			t.Errorf("For test #%d, the actual marshaled value is not what was expected.", testNumber)
+			t.Logf("EXPECTED: (%d)\n%s", len(expected), expected)
+			t.Logf("ACTUAL:   (%d)\n%s", len(actual), actual)
+			t.Logf("VALUE: (%T) %#v", test.Value, test.Value)
+			continue
+		}
+	}
+}
